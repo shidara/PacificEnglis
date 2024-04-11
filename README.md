ce45019fe61ad55cfb2ec12b3c9e14e7920e1a95
設楽開人 <kshidara@shitarainnomini.lan>
2024-04-11 19:33:27 +0900

    functionとgitignore調整

---
+++ README.md
@@ -0,0 +1,3 @@
+# PacificEnglis
+
+パシフィックイングリッシュ
--- welcart_basic-child/functions.php
+++ welcart_basic-child/functions.php
@@ -1,5 +1,7 @@
+<?php
 function theme_enqueue_styles() {
-wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-wp_enqueue_style( 'parent-cart', get_template_directory_uri() . '/usces_cart.css', array('parent-style', 'usces_default_css') );
+  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style('parent-cart', get_template_directory_uri() . '/usces_cart.css', array('parent-style', 'usces_default_css'));
 }
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+?>
