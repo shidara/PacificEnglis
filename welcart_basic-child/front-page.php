09edb5ab455e9061a63d4ead82fa88017e575b55
設楽開人 <kshidara@shitarainnomini.lan>
2024-04-16 01:54:12 +0900

    helloWorldの対応

--- welcart_basic-child/front-page.php
+++ welcart_basic-child/front-page.php
@@ -483,32 +483,36 @@
 
   <!-- Hello World -->
   <section class="HelloWorld">
-    <div class="HelloWorld__title">
-      <h3 class="HelloWorld__titleText">HelloWorld</h3>
-      <p class="HelloWorld__subTitle">一生モノの英語脳を身につけて、広い世界を体験しよう。</p>
-    </div>
-    <p class="HelloWorld__summary">お気軽にお問合せくださいお気軽にお問合せくださいお気軽にお問合せくださいお気軽にお問合せくださいお気軽にお問合せくださいお気軽にお問合せください</p>
+    <div class="HelloWorld__inner">
+      <div class="HelloWorld__title">
+        <h3 class="HelloWorld__titleText">HelloWorld</h3>
+        <p class="HelloWorld__subTitle">一生モノの英語脳を身につけて、広い世界を体験しよう。</p>
+      </div>
+      <p class="HelloWorld__summary">お気軽にお問合せくださいお気軽にお問合せくださいお気軽にお問合せくださいお気軽にお問合せくださいお気軽にお問合せくださいお気軽にお問合せください</p>
 
-    <div class="HelloWorld__tell">
-      <a href="tel:0975387350">
-        <p class="HelloWorld__tellTitle">日本人スタッフが対応します</p>
-        <div class="HelloWorld__tellNumber">
-          <span class="HelloWorld__tellNumberIcon"><img src="https://dummyimage.com/26x26/f27d53/fff.png" width="10" height="17" alt="" /></span>
-          <span class="HelloWorld__tellNumberText">097-538-7350</span>
-        </div>
-        <p class="HelloWorld__tellTime">受付時間 10:00-19:00</p>
-      </a>
-    </div>
-
-    <div class="HelloWorld__experience">
-      <a href="#">
-        <p class="HelloWorld__experienceTitle">&nbsp;&nbsp;&nbsp;最短翌日から可能！<br /><span class="HelloWorld__experienceTitleLarge">4</span>ステップで予約完了<span class="HelloWorld__experiencePin"><img src="https://dummyimage.com/38x38/f27d53/fff.png" width="38" height="38" alt="" /></span></p>
-        <div class="HelloWorld__experienceLine">
-          <span class="HelloWorld__experienceLineIcon"><img src="https://dummyimage.com/26x26/f27d53/fff.png" width="10" height="17" alt="" /></span>
-          <span class="HelloWorld__experienceLineText">無料体験入学</span>
+      <div class="HelloWorld__info">
+        <div class="HelloWorld__tell">
+          <a href="tel:0975387350">
+            <p class="HelloWorld__tellTitle">日本人スタッフが対応します</p>
+            <div class="HelloWorld__tellNumber">
+              <span class="HelloWorld__tellNumberIcon"><img src="https://dummyimage.com/26x26/f27d53/fff.png" width="10" height="17" alt="" /></span>
+              <span class="HelloWorld__tellNumberText">097-538-7350</span>
+            </div>
+            <p class="HelloWorld__tellTime">受付時間 10:00-19:00</p>
+          </a>
         </div>
 
-      </a>
+        <div class="HelloWorld__experience">
+          <a href="#">
+            <p class="HelloWorld__experienceTitle">&nbsp;&nbsp;&nbsp;最短翌日から可能！<br /><span class="HelloWorld__experienceTitleLarge">4</span>ステップで予約完了<span class="HelloWorld__experiencePin"><img src="https://dummyimage.com/38x38/f27d53/fff.png" width="38" height="38" alt="" /></span></p>
+            <div class="HelloWorld__experienceLine">
+              <span class="HelloWorld__experienceLineIcon"><img src="https://dummyimage.com/26x26/f27d53/fff.png" width="10" height="17" alt="" /></span>
+              <span class="HelloWorld__experienceLineText">無料体験入学</span>
+            </div>
+
+          </a>
+        </div>
+      </div>
     </div>
   </section>
 
