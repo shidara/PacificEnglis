ea4c8cc3cb8ed3eecc8b6992c8bba49758ca7cc1
shidara <k_kaito_1004@yahoo.co.jp>
2024-04-22 02:32:54 +0900

    オンライン英会話箱だけ作成

---
+++ welcart_basic-child/page-templates/online-english.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Online English
+ *
+ * @package Welcart
+ * @subpackage Welcart_Basic
+ */
+?>
+<?php get_header();?>
+
+<!-- コンテンツ -->
+<main class="pageContents RecruitPage">
+  <!-- Hello World -->
+  <?php get_template_part('helloWorld'); ?>
+
+  <!-- Recruit -->
+  <?php get_template_part('recruit'); ?>
+</main>
+
+<?php get_footer();?>
