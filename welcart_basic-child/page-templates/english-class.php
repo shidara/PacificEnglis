282a73ca748e6aee354abee4039f74971908a755
設楽開人 <kshidara@shitarainnomini.lan>
2024-04-21 15:13:22 +0900

    英語の教室ページのテンプレート用意

---
+++ welcart_basic-child/page-templates/english-class.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: English-class
+ *
+ * @package Welcart
+ * @subpackage Welcart_Basic
+ */
+?>
+<?php get_header();?>
+
+<?php get_template_part('mainImage'); ?>
+
+  <!-- コンテンツ -->
+  <main class="pageContents EnglishClass">
+    <!-- ページタイトル -->
+    <section class="PageTitleSection">
+      <p class="PageTitleSection__subText">ネイティブだから学べる英会話</p>
+      <h2 class="PageTitleSection__title PageTitleSection__title--small">英語の教室</h2>
+      <p class="PageTitleSection__summary PageTitleSection__summary--black">「学びの中に楽しさを感じれる」そのようなレッスンの提供をパシフィックイングリッシュは目指してます。弊校のカリキュラムチームは日々、日本の子供たちに最適なレッスンの構築に取り組んでおります。</p>
+    </section>
+  </main>
+
+<?php get_footer();?>
