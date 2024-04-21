242054ba2684624bd616125d061465a52e310c7c
設楽開人 <kshidara@shitarainnomini.lan>
2024-04-21 23:26:14 +0900

    講師紹介

--- welcart_basic-child/page-templates/instructor-list.php
+++ welcart_basic-child/page-templates/instructor-list.php
@@ -9,8 +9,177 @@
 <?php get_header();?>
 
 <!-- コンテンツ -->
- <main class="pageContents InstructorList">
+  <main class="pageContents Instructor">
 
- </main>
+    <!-- 講師紹介 -->
+    <section class="InstructorList">
+      <div class="InstructorList__title">
+        <h3 class="InstructorList__titleText">講師紹介</h3>
+      </div>
+
+      <div class="InstructorList__contents">
+        <div class="InstructorList__item">
+          <div class="InstructorList__itemThumbnail">
+            <img src="https://dummyimage.com/350x446/0f0/fff.png" alt="" loading="lazy" />
+          </div>
+
+          <p class="InstructorList__itemName">Eric ClarkEric Clark</p>
+          <p class="InstructorList__itemOneWord">レッスンで学んだことをどんどん話そう！</p>
+
+          <div class="InstructorList__itemFreeList">
+            <ul>
+              <li class="InstructorList__itemFreeListItem">
+                <p class="InstructorList__itemFreeTitle">出身</p>
+                <p class="InstructorList__itemFreeText">オーストラリア</p>
+              </li>
+              <li class="InstructorList__itemFreeListItem">
+                <p class="InstructorList__itemFreeTitle">趣味</p>
+                <p class="InstructorList__itemFreeText">キャンプ</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="InstructorList__item">
+          <div class="InstructorList__itemThumbnail">
+            <img src="https://dummyimage.com/350x446/0f0/fff.png" alt="" loading="lazy" />
+          </div>
+
+          <p class="InstructorList__itemName">Eric ClarkEric Clark</p>
+          <p class="InstructorList__itemOneWord">レッスンで学んだことをどんどん話そう！</p>
+
+          <div class="InstructorList__itemFreeList">
+            <ul>
+              <li class="InstructorList__itemFreeListItem">
+                <p class="InstructorList__itemFreeTitle">出身</p>
+                <p class="InstructorList__itemFreeText">オーストラリア</p>
+              </li>
+              <li class="InstructorList__itemFreeListItem">
+                <p class="InstructorList__itemFreeTitle">趣味</p>
+                <p class="InstructorList__itemFreeText">キャンプ</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="InstructorList__item">
+          <div class="InstructorList__itemThumbnail">
+            <img src="https://dummyimage.com/350x446/0f0/fff.png" alt="" loading="lazy" />
+          </div>
+
+          <p class="InstructorList__itemName">Eric ClarkEric Clark</p>
+          <p class="InstructorList__itemOneWord">レッスンで学んだことをどんどん話そう！</p>
+
+          <div class="InstructorList__itemFreeList">
+            <ul>
+              <li class="InstructorList__itemFreeListItem">
+                <p class="InstructorList__itemFreeTitle">出身</p>
+                <p class="InstructorList__itemFreeText">オーストラリア</p>
+              </li>
+              <li class="InstructorList__itemFreeListItem">
+                <p class="InstructorList__itemFreeTitle">趣味</p>
+                <p class="InstructorList__itemFreeText">キャンプ</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="InstructorList__item">
+          <div class="InstructorList__itemThumbnail">
+            <img src="https://dummyimage.com/350x446/0f0/fff.png" alt="" loading="lazy" />
+          </div>
+
+          <p class="InstructorList__itemName">Eric ClarkEric Clark</p>
+          <p class="InstructorList__itemOneWord">レッスンで学んだことをどんどん話そう！</p>
+
+          <div class="InstructorList__itemFreeList">
+            <ul>
+              <li class="InstructorList__itemFreeListItem">
+                <p class="InstructorList__itemFreeTitle">出身</p>
+                <p class="InstructorList__itemFreeText">オーストラリア</p>
+              </li>
+              <li class="InstructorList__itemFreeListItem">
+                <p class="InstructorList__itemFreeTitle">趣味</p>
+                <p class="InstructorList__itemFreeText">キャンプ</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="InstructorList__item">
+          <div class="InstructorList__itemThumbnail">
+            <img src="https://dummyimage.com/350x446/0f0/fff.png" alt="" loading="lazy" />
+          </div>
+
+          <p class="InstructorList__itemName">Eric ClarkEric Clark</p>
+          <p class="InstructorList__itemOneWord">レッスンで学んだことをどんどん話そう！</p>
+
+          <div class="InstructorList__itemFreeList">
+            <ul>
+              <li class="InstructorList__itemFreeListItem">
+                <p class="InstructorList__itemFreeTitle">出身</p>
+                <p class="InstructorList__itemFreeText">オーストラリア</p>
+              </li>
+              <li class="InstructorList__itemFreeListItem">
+                <p class="InstructorList__itemFreeTitle">趣味</p>
+                <p class="InstructorList__itemFreeText">キャンプ</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="InstructorList__item">
+          <div class="InstructorList__itemThumbnail">
+            <img src="https://dummyimage.com/350x446/0f0/fff.png" alt="" loading="lazy" />
+          </div>
+
+          <p class="InstructorList__itemName">Eric ClarkEric Clark</p>
+          <p class="InstructorList__itemOneWord">レッスンで学んだことをどんどん話そう！</p>
+
+          <div class="InstructorList__itemFreeList">
+            <ul>
+              <li class="InstructorList__itemFreeListItem">
+                <p class="InstructorList__itemFreeTitle">出身</p>
+                <p class="InstructorList__itemFreeText">オーストラリア</p>
+              </li>
+              <li class="InstructorList__itemFreeListItem">
+                <p class="InstructorList__itemFreeTitle">趣味</p>
+                <p class="InstructorList__itemFreeText">キャンプ</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="InstructorList__item">
+          <div class="InstructorList__itemThumbnail">
+            <img src="https://dummyimage.com/350x446/0f0/fff.png" alt="" loading="lazy" />
+          </div>
+
+          <p class="InstructorList__itemName">Eric ClarkEric Clark</p>
+          <p class="InstructorList__itemOneWord">レッスンで学んだことをどんどん話そう！</p>
+
+          <div class="InstructorList__itemFreeList">
+            <ul>
+              <li class="InstructorList__itemFreeListItem">
+                <p class="InstructorList__itemFreeTitle">出身</p>
+                <p class="InstructorList__itemFreeText">オーストラリア</p>
+              </li>
+              <li class="InstructorList__itemFreeListItem">
+                <p class="InstructorList__itemFreeTitle">趣味</p>
+                <p class="InstructorList__itemFreeText">キャンプ</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <!-- Hello World -->
+    <?php get_template_part('helloWorld'); ?>
+
+    <!-- Recruit -->
+    <?php get_template_part('recruit'); ?>
+  </main>
 
 <?php get_footer();?>
