9ec7c3a3464e96fe27487ef2f67e77202cbea512
shidara <k_kaito_1004@yahoo.co.jp>
2024-04-26 01:09:45 +0900

    カートのデザイン調整

--- welcart_basic-child/header.php
+++ welcart_basic-child/header.php
@@ -14,7 +14,7 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
   <meta name="format-detection" content="telephone=no"/>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
