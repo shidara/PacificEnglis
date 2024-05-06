6d3a33d1051e2d197c69f87f25c5422dac3857e0
shidara <k_kaito_1004@yahoo.co.jp>
2024-05-06 18:38:12 +0900

    LinksNavigation PC作成

--- welcart_basic-child/page-templates/english-class.php
+++ welcart_basic-child/page-templates/english-class.php
@@ -134,6 +134,45 @@
       </div>
     </section>
 
+    <!-- LinksNavigation PCのみ -->
+    <div class="LinksNavigation">
+      <ul>
+        <li class="LinksNavigation__item" style="background-image: url('<?php echo $imgUri;?>/23.webp');">
+          <a href="#">
+            <div>
+              <p class="LinksNavigation__itemTitle">Contact</p>
+              <p class="LinksNavigation__itemSubText">体験・予約する</p>
+            </div>
+            <div class="LinksNavigation__itemIcon">
+              <img src="<?php echo $imgUri;?>/arrow.webp" loading="lazy" />
+            </div>
+          </a>
+        </li>
+        <li class="LinksNavigation__item" style="background-image: url('<?php echo $imgUri;?>/23.webp');">
+          <a href="<?php echo esc_url( home_url( '/' ) ); ?>instructor-list/">
+            <div>
+              <p class="LinksNavigation__itemTitle">Teachers</p>
+              <p class="LinksNavigation__itemSubText">講師を見る</p>
+            </div>
+            <div class="LinksNavigation__itemIcon">
+              <img src="<?php echo $imgUri;?>/arrow.webp" loading="lazy" />
+            </div>
+          </a>
+        </li>
+        <li class="LinksNavigation__item" style="background-image: url('<?php echo $imgUri;?>/23.webp');">
+          <a href="#">
+            <div>
+              <p class="LinksNavigation__itemTitle">Company</p>
+              <p class="LinksNavigation__itemSubText">会社を知る</p>
+            </div>
+            <div class="LinksNavigation__itemIcon">
+              <img src="<?php echo $imgUri;?>/arrow.webp" loading="lazy" />
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+
     <!-- Hello World -->
     <?php get_template_part('helloWorld'); ?>
 
