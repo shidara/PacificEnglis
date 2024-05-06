9548ab99490f9ef0f693f7f0548f8a2655216352
Kaito Shidara <k_kaito_1004@yahoo.co.jp>
2024-05-06 15:31:10 +0900

    LessonMainのPC対応 (#84)

--- welcart_basic-child/page-templates/international-school.php
+++ welcart_basic-child/page-templates/international-school.php
@@ -11,6 +11,7 @@
 <?php get_header();?>
 
 <?php get_template_part('mainImage'); ?>
+<?php get_template_part('FixedNavigation'); ?>
 
   <!-- コンテンツ -->
   <main class="pageContents InternationalSchool">
