ddb1c58a7e8f03ddbbf27fd5f2a6739ae5d23774
Kaito Shidara <k_kaito_1004@yahoo.co.jp>
2024-05-16 23:57:21 +0900

    カレント時のナビの背景色調整 (#116)

--- welcart_basic-child/FixedNavigation.php
+++ welcart_basic-child/FixedNavigation.php
@@ -7,6 +7,40 @@
  */
 
   $imgUri = get_theme_file_uri() . "/assets/images/navigation/";
+
+  /**
+   * URLチェック
+   * インターナショナルスクール: 1
+   * 英語教室: 2
+   * オンライン英会話: 3
+   * その他: 0
+  */
+  $currentNum = 0;
+  if($_SERVER['REQUEST_URI']){
+    $accessUrl = $_SERVER['REQUEST_URI'];
+
+    if(strpos($accessUrl, 'international-school') !== false){
+      $currentNum = 1;
+    }elseif(strpos($accessUrl, 'english-class') !== false){
+      $currentNum = 2;
+    }elseif(strpos($accessUrl, 'online-english') !== false){
+      $currentNum = 3;
+    }
+  }
+
+  // クラス名の変更
+  $internationalSchoolClass = 'FixedNavigation__item';
+  $englishClass = 'FixedNavigation__item';
+  $onlineEnglish = 'FixedNavigation__item';
+  if($currentNum > 0){
+    if($currentNum === 1){
+      $internationalSchoolClass = 'FixedNavigation__item FixedNavigation__item--active';
+    } elseif($currentNum === 2){
+      $englishClass = 'FixedNavigation__item FixedNavigation__item--active';
+    } elseif($currentNum === 3){
+      $onlineEnglish = 'FixedNavigation__item FixedNavigation__item--active';
+    }
+  }
 ?>
 
 <!-- PC用ナビゲーション -->
@@ -18,7 +52,7 @@
       </a>
     </li>
 
-    <li class="FixedNavigation__item">
+    <li class="<?php echo $internationalSchoolClass;?>">
       <a href="<?php echo esc_url( home_url( '/' ) ); ?>international-school/">
         <img src="<?php echo $imgUri;?>/nav-02.webp" alt="" width="170" height="72" class="FixedNavigation__itemThumbnail" />
         <div class="FixedNavigation__itemTitle">
@@ -28,7 +62,7 @@
       </a>
     </li>
 
-    <li class="FixedNavigation__item">
+    <li class="<?php echo $englishClass;?>">
       <a href="<?php echo esc_url( home_url( '/' ) ); ?>english-class/">
         <img src="<?php echo $imgUri;?>/experience.webp" alt="" width="170" height="72" class="FixedNavigation__itemThumbnail" />
         <div class="FixedNavigation__itemTitle">
@@ -38,7 +72,7 @@
       </a>
     </li>
 
-    <li class="FixedNavigation__item">
+    <li class="<?php echo $onlineEnglish;?>">
       <a href="<?php echo esc_url( home_url( '/' ) ); ?>online-english/">
         <img src="<?php echo $imgUri;?>/nav-01.webp" alt="" width="170" height="72" class="FixedNavigation__itemThumbnail" />
         <div class="FixedNavigation__itemTitle">
