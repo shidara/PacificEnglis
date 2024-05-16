6388715d7eeb980409387b0d98a525885e5b6bb5
Kaito Shidara <k_kaito_1004@yahoo.co.jp>
2024-05-16 23:28:40 +0900

    iインジケーターの作成 (#115)

--- welcart_basic-child/mainImage.php
+++ welcart_basic-child/mainImage.php
@@ -6,47 +6,22 @@
  * @subpackage Welcart_Basic
  */
   $imgUri = get_theme_file_uri() . "/assets/images/top/";
+  $imageList = ['01.webp','02.webp','03.webp','04.webp']
 ?>
 
 <div class="MainImage">
   <div class="MainImage__inner">
     <div class="MainImageSlider swiper js-homeMainVisualSlider">
       <div class="swiper-wrapper">
-        <div class="swiper-slide">
-          <picture>
-            <!-- <source srcset="https://dummyimage.com/350x530/f27d53/fff.png" media="(min-width:600px)" type="image/webp">
-            <source srcset="<?php echo $imgUri; ?>corporate/xl_mainvisual-1.jpg" media="(min-width:600px)">
-            <source srcset="<?php echo $imgUri; ?>corporate/sm_mainvisual-1.webp" type="image/webp"> -->
-            <img src="<?php echo $imgUri;?>/01.webp" alt="">
-          </picture>
-        </div>
-        <div class="swiper-slide">
-          <picture>
-            <!-- <source srcset="<?php echo $imgUri; ?>corporate/xl_mainvisual-2.webp" media="(min-width:600px)" type="image/webp">
-            <source srcset="<?php echo $imgUri; ?>corporate/xl_mainvisual-2.jpg" media="(min-width:600px)">
-            <source srcset="<?php echo $imgUri; ?>corporate/sm_mainvisual-2.webp" type="image/webp"> -->
-            <img src="<?php echo $imgUri;?>/02.webp" alt="">
-          </picture>
-        </div>
-
-        <div class="swiper-slide">
-          <picture>
-            <!-- <source srcset="<?php echo $imgUri; ?>corporate/xl_mainvisual-2.webp" media="(min-width:600px)" type="image/webp">
-            <source srcset="<?php echo $imgUri; ?>corporate/xl_mainvisual-2.jpg" media="(min-width:600px)">
-            <source srcset="<?php echo $imgUri; ?>corporate/sm_mainvisual-2.webp" type="image/webp"> -->
-            <img src="<?php echo $imgUri;?>/03.webp" alt="">
-          </picture>
-        </div>
-
-        <div class="swiper-slide">
-          <picture>
-            <!-- <source srcset="<?php echo $imgUri; ?>corporate/xl_mainvisual-2.webp" media="(min-width:600px)" type="image/webp">
-            <source srcset="<?php echo $imgUri; ?>corporate/xl_mainvisual-2.jpg" media="(min-width:600px)">
-            <source srcset="<?php echo $imgUri; ?>corporate/sm_mainvisual-2.webp" type="image/webp"> -->
-            <img src="<?php echo $imgUri;?>/04.webp" alt="">
-          </picture>
-        </div>
-        <div class="progress js-progress"><span></span></div>
+        <?php
+          foreach($imageList as $value) {
+            echo '<div class="swiper-slide">
+                    <picture>
+                      <img src="' . $imgUri . $value . '" alt="">
+                    </picture>
+                  </div>';
+          }
+        ?>
       </div>
 
       <div class="MainImage__info">
@@ -54,6 +29,17 @@
           <p class="MainImage__infoTitle">英語の保育園</p>
         </div>
         <div><img src="<?php echo $imgUri;?>/arrow-wht.webp" alt="" width="32" height="32" /></div>
+      </div>
+
+    </div>
+
+    <div class="MainImage__progress">
+      <div class="progress js-progress">
+        <?php
+          foreach($imageList as $value) {
+            echo '<span></span>';
+          }
+        ?>
       </div>
     </div>
   </div>
